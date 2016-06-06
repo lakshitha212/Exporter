2ee9a93733255cd95e0f99aa61833b861609a9fe
Chameera Lakshitha <chameera@encyteus.com>
2016-06-06 11:34:09 +0530

    Change js

--- js/exporter.js
+++ js/exporter.js
@@ -14,10 +14,32 @@
             $("<div/>", {
                 class: "row " + settings._align
             }).append(
-                $("<div/>", {
-                    text: "Hello World",
-                    class: "col-md-12"
-                })
+                $("<div/>", {class: "col-md-11"}),
+                $("<div/>", {class: "col-md-1 "}).append(
+                    $("<div/>", {class: "row"}).append(
+                        $("<div/>", {class: "col-md-12 form-group"}).append(
+                            $("<i/>", {
+                                class: "fa fa-file-excel-o",
+                                title: "Excel",
+                                click: function () {
+                                    console.log("Excel");
+                                }
+                            }).css({
+                                "padding-right": "5px",
+                                "cursor": "pointer"
+                            }),
+                            $("<i/>", {
+                                class: "fa fa-file-pdf-o",
+                                title: "PDF",
+                                click: function () {
+                                    console.log("PDF");
+                                }
+                            }).css({
+                                "cursor": "pointer"
+                            })
+                        )
+                    )
+                )
             )
         );
     };
