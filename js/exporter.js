5a8d1bd7cc2a0c5eae1356147059c61190f9bf6b
Chameera Lakshitha <chameera@encyteus.com>
2016-06-06 12:31:50 +0530

    finalize

--- js/exporter.js
+++ js/exporter.js
@@ -5,11 +5,32 @@
 (function ($) {
     'use strict';
     $.fn.exporter = function (options) {
-        var settings = $.extend({
-            _id: "",
-            _align: "text-right"
-        }, options);
-        console.log(settings);
+        var _display_xl,
+            _display_pdf,
+            settings = $.extend({
+                _id: "",
+                _align: "text-right",
+                _type: [1, 2],
+                _file: "Report"
+            }, options);
+        /**
+         * Manage Options
+         * 1 : EXL
+         * 2 : PDF
+         */
+        if (settings._type.length > 0) {
+            if (settings._type.length != 2) {
+                if (!$.inArray(1, settings._type)) {
+                    _display_xl = "none";
+                }
+                if (!$.inArray(2, settings._type)) {
+                    _display_pdf = "none";
+                }
+            }
+        } else {
+            _display_xl = "none";
+            _display_pdf = "none";
+        }
         $('#content_tbl').prepend(
             $("<div/>", {
                 class: "row " + settings._align
@@ -22,20 +43,37 @@
                                 class: "fa fa-file-excel-o",
                                 title: "Excel",
                                 click: function () {
-                                    console.log("Excel");
+                                    var uri = '';
+                                    if (navigator.appVersion.indexOf("Win") != -1) {
+                                        uri = 'data:application/vnd.ms-excel;base64,';
+                                    }
+                                    if (navigator.appVersion.indexOf("Linux") != -1) {
+                                        uri = 'data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;base64,';
+                                    }
+                                    var template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>Worksheet</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>' + $("#" + settings._id).find('table').html() + '</table></body></html>',
+                                        a = document.createElement('a');
+                                    a.href = uri + window.btoa(unescape(encodeURIComponent(template)));
+                                    a.setAttribute('download', settings._file + '.xls');
+                                    a.click();
                                 }
                             }).css({
                                 "padding-right": "5px",
-                                "cursor": "pointer"
+                                "cursor": "pointer",
+                                "display": _display_xl
                             }),
                             $("<i/>", {
                                 class: "fa fa-file-pdf-o",
                                 title: "PDF",
                                 click: function () {
-                                    console.log("PDF");
+                                    var page = '<html><table border="1" style="border-spacing:0;width:100%">' + $("#" + settings._id).find('table').html() + '</table></html>';
+                                    var w = window.open();
+                                    w.document.write(page);
+                                    w.print();
+                                    w.close();
                                 }
                             }).css({
-                                "cursor": "pointer"
+                                "cursor": "pointer",
+                                "display": _display_pdf
                             })
                         )
                     )
